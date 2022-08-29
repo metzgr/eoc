63ffb325e39a3b11b94edec692f4155d9eb8ce11
HarikaMunipalle <harika.munipalle@gsa.gov>
2022-08-29 12:08:15 -0400

    ECASOGP-6415-fix-footer-button

--- _assets/js/app.js
+++ _assets/js/app.js
@@ -6,6 +6,9 @@ $("a").each(function(index, element) {
     if(!$(element).attr("href").startsWith('https://www.evaluation.gov' && 'javascript:void(0)') 
         && !$(element).hasClass('usa-link--external')
         && this.host !== location.host){
+        if ($(element).hasClass('usa-link--external-white'))
+            $(element).addClass("usa-link--external-white");
+        else
             $(element).addClass("usa-link--external");
         }
 });
