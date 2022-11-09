eeb41b86452f2ca3bd155f845a5920aaa299a4d6
HarikaMunipalle <harika.munipalle@gsa.gov>
2022-11-09 14:33:50 -0500

    removed javascript

--- _assets/js/app.js
+++ _assets/js/app.js
@@ -3,7 +3,7 @@ console.log("Hi from Federalist");
 
 // Add a new class for all of the external anchor tags
 $("a").each(function(index, element) {
-    if(!$(element).attr("href").startsWith('https://www.evaluation.gov' && 'javascript:void(0)') 
+    if(!$(element).attr("href").startsWith('https://www.evaluation.gov') 
         && !$(element).hasClass('usa-link--external')
         && this.host !== location.host){
         if ($(element).hasClass('usa-link--external-white'))
